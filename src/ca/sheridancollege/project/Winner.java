3dc714cb172d80b3e7d4a75a6a879ca4e07ca3d8
ashish3012 <pate2159@sheridancollege.ca>
2020-02-05 15:50:14 -0500

    Deliverable 1

---
+++ src/ca/sheridancollege/project/Winner.java
@@ -0,0 +1,31 @@
+
+package ca.sheridancollege.project;
+
+
+public class Winner {
+    
+    private String winnerName;
+
+    /**
+     * @return the winnerName
+     */
+    public String getWinnerName() {
+        return winnerName;
+    }
+
+    /**
+     * @param winnerName the winnerName to set
+     */
+    public void setWinnerName(String winnerName) {
+        this.winnerName = winnerName;
+    }
+    
+    /**
+     * this method is called to declare winner
+     */
+    
+    public void winner()
+    {
+        
+    }
+}
